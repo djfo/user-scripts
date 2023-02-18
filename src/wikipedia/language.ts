4ab00483c11c45c0514592a823faf8d9edf55414
David Obwaller <david.obwaller@gmail.com>
2023-02-18 10:37:24 +0100

    Organized code into functions

--- src/wikipedia/language.ts
+++ src/wikipedia/language.ts
@@ -5,58 +5,86 @@
 // @include https://*.wikipedia.org/wiki/*
 // ==/UserScript==
 
-const langs = ["en", "de", "fr", "it"];
-
-const links = [...document.querySelectorAll(".interlanguage-link a")] as HTMLAnchorElement[];
-
-const map = new Map(links.flatMap(link => {
-  const href = link.href;
-  const lang = link.lang;
-  const record = { lang, href };
-  return langs.includes(lang) ? [[lang, record]] : [];
-}));
-
-const keyCode1 = 0x31;
-const bindings = new Map(langs.map((lang, index) => [keyCode1 + index, lang]));
-
-const lis = langs.map((lang, index) => {
-  const value = map.get(lang);
-  const li = document.createElement("li");
-  li.setAttribute("style", "margin: 0; padding: 0");
-  const textNode = document.createTextNode(`${index + 1} ${lang}`);
-  if (value !== undefined) {
-    const { href } = value;
-    const a = document.createElement("a");
-    a.appendChild(textNode);
-    a.href = href;
-    li.appendChild(a);
+function getInterLanguageLinks(): HTMLAnchorElement[] {
+  const elements = [...document.querySelectorAll(".interlanguage-link a")];
+  if (elements.every((element) => element instanceof HTMLAnchorElement)) {
+    return elements as HTMLAnchorElement[];
   } else {
-    li.appendChild(textNode);
+    throw new Error("Expected HTML anchor elements.");
   }
-  return li;
-});
-const div = document.createElement("div");
-div.setAttribute("style", "position: fixed; top: 0; right: 0; background: yellow");
-const ul = document.createElement("ul");
-ul.setAttribute("style", "list-style: none; margin: 0; padding: 0");
-ul.append(...lis);
-div.appendChild(ul);
-document.body.appendChild(div);
+}
 
-document.addEventListener('keydown', (ev) => {
-  const activeElement = document.activeElement;
+type M = Map<string, { lang: string; href: string }>;
 
-  if (activeElement !== document.body) {
-    return;
-  }
+function makeMap(links: HTMLAnchorElement[], langs: string[]): M {
+  return new Map(
+    links.flatMap((link) => {
+      const href = link.href;
+      const lang = link.lang;
+      const record = { lang, href };
+      return langs.includes(lang) ? [[lang, record]] : [];
+    })
+  );
+}
 
-  const binding = bindings.get(ev.keyCode);
-
-  if (binding !== undefined) {
-    const lang = binding;
+function makeLanguageMenuHtml(langs: string[], map: M) {
+  const lis = langs.map((lang, index) => {
     const value = map.get(lang);
+    const li = document.createElement("li");
+    li.setAttribute("style", "margin: 0; padding: 0");
+    const textNode = document.createTextNode(`${index + 1} ${lang}`);
     if (value !== undefined) {
-      window.location.replace(value.href);
+      const { href } = value;
+      const a = document.createElement("a");
+      a.appendChild(textNode);
+      a.href = href;
+      li.appendChild(a);
+    } else {
+      li.appendChild(textNode);
     }
-  }
-});
+    return li;
+  });
+  const div = document.createElement("div");
+  div.setAttribute(
+    "style",
+    "position: fixed; top: 0; right: 0; background: yellow"
+  );
+  const ul = document.createElement("ul");
+  ul.setAttribute("style", "list-style: none; margin: 0; padding: 0");
+  ul.append(...lis);
+  div.appendChild(ul);
+  return div;
+}
+
+function init() {
+  const langs = ["en", "de", "fr", "it"];
+  const keyCode1 = 0x31;
+  const links = getInterLanguageLinks();
+  const map = makeMap(links, langs);
+
+  const div = makeLanguageMenuHtml(langs, map);
+  document.body.appendChild(div);
+
+  const bindings = new Map(
+    langs.map((lang, index) => [keyCode1 + index, lang])
+  );
+  document.addEventListener("keydown", (ev) => {
+    const activeElement = document.activeElement;
+
+    if (activeElement !== document.body) {
+      return;
+    }
+
+    const binding = bindings.get(ev.keyCode);
+
+    if (binding !== undefined) {
+      const lang = binding;
+      const value = map.get(lang);
+      if (value !== undefined) {
+        window.location.replace(value.href);
+      }
+    }
+  });
+}
+
+init();
